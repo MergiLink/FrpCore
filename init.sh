34aae029bbce77100fcbf2e5f2ab5bcdf981b0c7
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2024-06-29 00:39:43 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-LOCAL_FILES_DIR="./*"
+LOCAL_FILES_DIR=$(pwd)
 REPO_URL="https://github.com/fatedier/frp.git"
 REPO_DIR="/tmp/frp"
 
-if [ -d "src" ]; then
-rm -rf src
-mkdir src
+if [ -d "source" ]; then
+rm -rf source
 fi
+mkdir source
 
 # 克隆 GitHub 仓库
 if [ -d "$REPO_DIR" ]; then
@@ -18,7 +18,7 @@ git clone "$REPO_URL" "$REPO_DIR"
 cd "$REPO_DIR" || exit 1
 
 # 将本地文件复制到仓库目录
-cp -r "$LOCAL_FILES_DIR" "$REPO_DIR"
+cp -r "$LOCAL_FILES_DIR"/* "$REPO_DIR"
 rm -rf init.sh
 
 # 检查并合并文件
@@ -29,12 +29,12 @@ git commit -m "合并代码" || {
   echo "没有修改"
 }
 
-cp -r "$REPO_DIR"/* "$LOCAL_FILES_DIR"/src
+cp -r "$REPO_DIR"/* "$LOCAL_FILES_DIR"/source
 
 # 解决冲突（如果有）
 if git status | grep -q "conflict"; then
   git status
 else
- echo "cd src"
+ echo "cd source"
  echo "make"
 fi
